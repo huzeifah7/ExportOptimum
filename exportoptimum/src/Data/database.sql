32bccb59e64261b3fb6d22a6ce69373d50da2fd5
Houdaifa <houdayfa.2016@gmail.com>
2025-04-11 12:52:35 +0200

    updated data Sql with the dumped data

--- exportoptimum/src/Data/database.sql
+++ exportoptimum/src/Data/database.sql
@@ -16,24 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `assurances`
---
-
-DROP TABLE IF EXISTS `assurances`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `assurances` (
-  `idAssurance` int NOT NULL AUTO_INCREMENT,
-  `IdParagraph` int DEFAULT NULL,
-  `AssuranceName` varchar(255) DEFAULT NULL,
-  `AssuranceImage` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`idAssurance`),
-  KEY `IdParagraph` (`IdParagraph`),
-  CONSTRAINT `assurances_ibfk_1` FOREIGN KEY (`IdParagraph`) REFERENCES `qualityassuranceparagraphs` (`IdParagraph`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `assurances`
 --
 
@@ -42,26 +24,6 @@ LOCK TABLES `assurances` WRITE;
 INSERT INTO `assurances` VALUES (1,1,'BRC Food test','/BRC.png'),(2,1,'Global GAP','/GLOBALGAP.png'),(3,1,'BIO Certification','/BIO.jpg'),(4,1,'ISO Certified test','/spring.png');
 /*!40000 ALTER TABLE `assurances` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `blogs`
---
-
-DROP TABLE IF EXISTS `blogs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `blogs` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `date` date NOT NULL,
-  `summary` text NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `text` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `blogs`
@@ -74,22 +36,6 @@ INSERT INTO `blogs` VALUES (1,'First Blog Post','Author One','2023-12-31','This 
 UNLOCK TABLES;
 
 --
--- Table structure for table `key_figures`
---
-
-DROP TABLE IF EXISTS `key_figures`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `key_figures` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  `value` int NOT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `key_figures`
 --
 
@@ -100,22 +46,14 @@ INSERT INTO `key_figures` VALUES (1,'Tons of yearly export',50000,'tons'),(2,'Em
 UNLOCK TABLES;
 
 --
--- Table structure for table `products`
+-- Dumping data for table `messages`
 --
 
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `description` text,
-  `MoreDescription` text,
-  `mainImage` varchar(255) DEFAULT NULL,
-  `images` json DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'John Doe','john@example.com','','Hello','This is a test message');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Dumping data for table `products`
@@ -123,23 +61,9 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Hass','Fresh avocado from organic farms.','Hass avocados are celebrated for their creamy texture and nutty flavor, making them the preferred choice for culinary enthusiasts worldwide. They are rich in essential nutrients such as healthy fats, fiber, and potassium, supporting heart health and overall wellness. With their distinct pebbly skin and vibrant green flesh, Hass avocados are versatile and ideal for a wide range of dishes, from guacamole to salads and even smoothies. Carefully cultivated in organic farms, these avocados guarantee freshness, quality, and sustainability, providing a gourmet experience with every bite.','/ProductsImages/Hass.jpg','[\"/ProductsImages/Hass1.jpg\", \"/ProductsImages/Hass2.jpg\", \"/ProductsImages/Hass3.jpg\"]'),(2,'Fuerte','Creamy and ripe avocados.','The Fuerte avocado is a timeless classic, loved for its smooth, buttery texture and mild, nutty flavor. This variety is characterized by its thin, green skin, making it easy to peel and enjoy. Rich in vitamins, antioxidants, and healthy fats, Fuerte avocados are a nutritional powerhouse. Whether sliced into a salad, blended into a creamy dip, or spread on toast, they offer a versatile and delicious addition to any meal. Our Fuerte avocados are cultivated with care, ensuring freshness and exceptional quality for all your culinary needs.','/ProductsImages/Fuerte.jpg','[\"/ProductsImages/Fuerte1.jpg\", \"/ProductsImages/Fuerte2.jpg\", \"/ProductsImages/Fuerte3.jpg\"]'),(3,'Zutano','Perfect for guacamole.','Zutano avocados are known for their light, mild flavor and smooth texture, making them a refreshing choice for salads, salsas, and guacamole. Their glossy, thin skin and pear-like shape add a unique appeal. Packed with essential nutrients such as vitamins C, E, and B6, Zutano avocados provide a healthy boost to your diet. Grown in the finest orchards, these avocados are carefully handpicked to ensure premium quality and freshness. Enjoy the delicate taste and versatile nature of Zutano avocados, perfect for enhancing your favorite dishes.','/ProductsImages/Zutano.jpg','[\"/ProductsImages/Zutano1.jpg\", \"/ProductsImages/Zutano2.jpg\", \"/ProductsImages/Zutano3.jpg\"]'),(4,'Bacon less','Smooth and creamy texture. Less','Bacon avocados are renowned for their buttery smooth texture and mild flavor, making them a favorite choice for avocado lovers. These avocados are slightly larger and have a glossy, thin green skin, which is easy to peel. Packed with essential nutrients like potassium and folate, Bacon avocados are a healthy and tasty addition to your diet. Ideal for spreading, slicing, or mashing, they enhance everything from morning toast to gourmet dishes. Each Bacon avocado is grown with care to ensure premium quality and exceptional taste. LEss','/ProductsImages/1740339387245-1150062.jpg','[\"/ProductsImages/1740339387272-pngwing.com (6).png\", \"/ProductsImages/1740339387293-pngwing.com (5).png\", \"/ProductsImages/1740339387314-pngwing.com (4).png\"]'),(5,'Reed','A large and flavorful avocado.','Reed avocados are prized for their large size, creamy texture, and bold flavor. These round avocados have thick, green skin that protects the vibrant yellow-green flesh within. Known for their exceptional creaminess, Reed avocados are perfect for creating luxurious guacamole, spreads, or desserts. They are packed with monounsaturated fats, vitamins, and antioxidants, promoting heart health and overall well-being. Grown with dedication to quality, Reed avocados offer a delightful eating experience that satisfies the palate and nourishes the body.','/ProductsImages/Reed.jpg','[\"/ProductsImages/Reed1.jpg\", \"/ProductsImages/Reed2.jpg\", \"/ProductsImages/Reed3.jpg\"]'),(8,'Pinkton ','Good Avocado ','Here','/ProductsImages/1740340408912-âPngtreeâfresh avocado transparent background_15744100 (1).png','[\"/ProductsImages/1740340409022-pngwing.com (1).png\", \"/ProductsImages/1740340409049-pngwing.com.png\", \"/ProductsImages/1740340409103-âPngtreeâphoto of avocado_8939900.png\"]');
+INSERT INTO `products` VALUES (1,'Hass','Fresh avocado from organic farms.','Hass avocados are celebrated for their creamy texture and nutty flavor, making them the preferred choice for culinary enthusiasts worldwide. They are rich in essential nutrients such as healthy fats, fiber, and potassium, supporting heart health and overall wellness. With their distinct pebbly skin and vibrant green flesh, Hass avocados are versatile and ideal for a wide range of dishes, from guacamole to salads and even smoothies. Carefully cultivated in organic farms, these avocados guarantee freshness, quality, and sustainability, providing a gourmet experience with every bite.','/ProductsImages/Hass.jpg','[\"/ProductsImages/Hass1.jpg\", \"/ProductsImages/Hass2.jpg\", \"/ProductsImages/Hass3.jpg\"]'),(2,'Fuerte','Creamy and ripe avocados.','The Fuerte avocado is a timeless classic, loved for its smooth, buttery texture and mild, nutty flavor. This variety is characterized by its thin, green skin, making it easy to peel and enjoy. Rich in vitamins, antioxidants, and healthy fats, Fuerte avocados are a nutritional powerhouse. Whether sliced into a salad, blended into a creamy dip, or spread on toast, they offer a versatile and delicious addition to any meal. Our Fuerte avocados are cultivated with care, ensuring freshness and exceptional quality for all your culinary needs.','/ProductsImages/Fuerte.jpg','[\"/ProductsImages/Fuerte1.jpg\", \"/ProductsImages/Fuerte2.jpg\", \"/ProductsImages/Fuerte3.jpg\"]'),(3,'Zutano','Perfect for guacamole.','Zutano avocados are known for their light, mild flavor and smooth texture, making them a refreshing choice for salads, salsas, and guacamole. Their glossy, thin skin and pear-like shape add a unique appeal. Packed with essential nutrients such as vitamins C, E, and B6, Zutano avocados provide a healthy boost to your diet. Grown in the finest orchards, these avocados are carefully handpicked to ensure premium quality and freshness. Enjoy the delicate taste and versatile nature of Zutano avocados, perfect for enhancing your favorite dishes.','/ProductsImages/Zutano.jpg','[\"/ProductsImages/Zutano1.jpg\", \"/ProductsImages/Zutano2.jpg\", \"/ProductsImages/Zutano3.jpg\"]'),(5,'Reed','A large and flavorful avocado.','Reed avocados are prized for their large size, creamy texture, and bold flavor. These round avocados have thick, green skin that protects the vibrant yellow-green flesh within. Known for their exceptional creaminess, Reed avocados are perfect for creating luxurious guacamole, spreads, or desserts. They are packed with monounsaturated fats, vitamins, and antioxidants, promoting heart health and overall well-being. Grown with dedication to quality, Reed avocados offer a delightful eating experience that satisfies the palate and nourishes the body.','/ProductsImages/Reed.jpg','[\"/ProductsImages/Reed1.jpg\", \"/ProductsImages/Reed2.jpg\", \"/ProductsImages/Reed3.jpg\"]'),(8,'Pinkton ','Good Avocado ','Here','/ProductsImages/1740340408912-âPngtreeâfresh avocado transparent background_15744100 (1).png','[\"/ProductsImages/1740340409022-pngwing.com (1).png\", \"/ProductsImages/1740340409049-pngwing.com.png\", \"/ProductsImages/1740340409103-âPngtreeâphoto of avocado_8939900.png\"]');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `qualityassuranceparagraphs`
---
-
-DROP TABLE IF EXISTS `qualityassuranceparagraphs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `qualityassuranceparagraphs` (
-  `IdParagraph` int NOT NULL AUTO_INCREMENT,
-  `description` text,
-  PRIMARY KEY (`IdParagraph`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `qualityassuranceparagraphs`
@@ -150,26 +74,6 @@ LOCK TABLES `qualityassuranceparagraphs` WRITE;
 INSERT INTO `qualityassuranceparagraphs` VALUES (1,'We start our quality control process from the downstream of the supply chain. Before picking the fruit from the farm we send our quality specialist to approve the parcel based on our costumer specifications and tolerance , fruit is controlled one more time at reception and again during production process, the lot is only liberated if it does meet all costumer requirements. For export optimum quality is the key of continuity.');
 /*!40000 ALTER TABLE `qualityassuranceparagraphs` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `profile_pic` varchar(255) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
@@ -190,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-07 15:53:46
+-- Dump completed on 2025-04-11 12:43:40
