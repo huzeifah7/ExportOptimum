9538269c2247c6ddc13401150b516d9e8e098f28
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2025-11-24 15:02:25 +0000

    make the pink color display with white

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -160,7 +160,7 @@ export default function ContactPage() {
           </div>
         </section>
 
-        <section className="py-16 lg:py-24 bg-background">
+        <section className="py-16 lg:py-24 bg-white">
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-12 text-center">
                     <div className="flex flex-col items-center">
