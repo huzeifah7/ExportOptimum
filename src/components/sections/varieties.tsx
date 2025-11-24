0a81818157cf466c9e816274b459c9bc6f7f2bd1
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2025-11-24 21:00:22 +0000

    smaller

--- src/components/sections/varieties.tsx
+++ src/components/sections/varieties.tsx
@@ -30,7 +30,7 @@ export default function Varieties() {
             linear-gradient(to right, #d1d5db 1px, transparent 1px),
             linear-gradient(to bottom, #d1d5db 1px, transparent 1px)
           `,
-          backgroundSize: "96px 96px",
+          backgroundSize: "48px 48px",
           WebkitMaskImage:
             "radial-gradient(ellipse 60% 60% at 50% 50%, #000 30%, transparent 70%)",
           maskImage:
