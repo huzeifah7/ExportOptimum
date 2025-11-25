64817ae2896748affc769a1c6753d6e3b5bddbd7
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2025-11-25 12:40:45 +0000

    now, on Meet our team button on the CEO section , use replace the exist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=PT+Sans:wght@400;700&family=Nunito:wght@400;700;800&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body className="font-body antialiased bg-background">
         {children}
