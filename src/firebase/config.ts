49f064aa0b232952dac0ac962345acc954995869
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2025-11-30 13:27:58 +0000

    i got this error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7624884363-5c840",
   "appId": "1:933658333314:web:b8895e645f4c4617b4649b",
+  "storageBucket": "studio-7624884363-5c840.appspot.com",
   "apiKey": "AIzaSyCmTi9peoGochhX_trevziHNhpBgapEeFw",
   "authDomain": "studio-7624884363-5c840.firebaseapp.com",
   "measurementId": "",
