385a5352c0aeeaa81d327ae8a810e8c4fd481954
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2025-12-02 12:57:46 +0000

    on the page of Contact us the add a margin top because the header is hid

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React, { useState, useEffect } from 'react';
@@ -68,7 +69,7 @@ const Contact = () => {
   return (
     <div className="bg-background">
       <Header />
-      <div className="contact-container flex flex-col md:flex-row bg-background">
+      <div className="contact-container flex flex-col md:flex-row bg-background mt-24">
         <div className="imgContactBg"></div>
 
         <InView triggerOnce>
