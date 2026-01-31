c60d8bf6539548778618b7eac1d0b56bf129a52a
Houdaifa ELBAAL <houdayfa.2016@gmail.com>
2026-01-31 13:15:21 +0000

    the image doesn't displayed

--- src/app/quality/page.tsx
+++ src/app/quality/page.tsx
@@ -118,7 +118,7 @@ export default function QualityPage() {
             
             <main className="flex-grow relative z-10">
               {/* Hero Section */}
-              <section className="relative pt-2 pb-24 lg:pt-40 lg:pb-32 flex items-center justify-center overflow-hidden">
+              <section className="relative pt-32 pb-24 lg:pt-40 lg:pb-32 flex items-center justify-center text-white overflow-hidden">
                 <Image
                     src="/bgqUAQLITY.PNG"
                     alt="Quality avocados in a box"
